464980f2d8c168bc8b0d3c871dba069f6ab4c5cd
Dafeigy <1070642765@qq.com>
2023-04-28 16:04:30 +0800

    <Update>:update README.md

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 
 <p align="center">
-    <img src="https://raw.githubusercontent.com/dafeigy/image/master/20210301120514.png" alt="logo" width=500 height=500 />
+    <img src="Light.png" alt="logo" width=300 height=300 />
 </p>
 <h1 align="center">🌥WindPlayer🌥</h1>
 <p align="center">
@@ -33,36 +33,36 @@
 
 # 🎵这是什么
 
-这是一款基于PyQt5开发的音乐播放器，接入了网易云抖音榜API完成曲目推送。*~~其实这是一个为了后续的音乐播放器开发的一个简易版本~~。*
-
-对像我一样注意力容易分散的人，这款播放器很适合你~
-
-在考研日期正式确定后会再有一次更新，之后就会投入时间精力到考研中去了┗|｀O′|┛
+这是一款基于PyQt5开发的音乐播放器，接入了网易云抖音榜API完成曲目推送。设计风格非常简洁，如果不是因为课程作业的缘故还可以再简化一下UI界面。
+时间过得好快，没想到已经考研成功了。这次重新弄回这个这个其实是因为课程作业的缘故，毕竟代码量摆在那里还挺难从头开发一个Qt程序的。
+<p align="center">
+    <img src="./imgs/DEMO.png" alt="LightMode" width=800 height=400 />
+</p>
 
 # ⭐特点
 
 * **极简UI** 傻瓜式操作 ~~让你的工作环境逼格变高~~
 * **支持亮/暗模式**，契合当前桌面风格，**保留了一贯的半透明风格**
-* **不显示任何歌名、歌词、评论**，提高工作专注度
-* **不能快进与循环**，但可以切换当前曲目。**每首歌在单次运行时之播放一次。**
+* **不显示任何歌词、评论**，提高工作专注度
+* **不能快进与循环**，但可以切换当前曲目。**每首歌在单次运行时只播放一次。**
 
 # 📸运行截图
 
 ![]()
 
 <p align="center">
-    <img src="https://raw.githubusercontent.com/dafeigy/image/master/20210301120235.png" alt="DarkMode" width=400 height=200 />
+    <img src="./imgs/dark.png" alt="DarkMode" width=400 height=200 />
 </p>
 <h1 align="center">黑暗模式</h1>
 
 <p align="center">
-    <img src="https://raw.githubusercontent.com/dafeigy/image/master/20210301120442.png" alt="LightMode" width=400 height=200 />
+    <img src="./imgs/light.png" alt="LightMode" width=400 height=200 />
 </p>
 <h1 align="center">光亮模式</h1>
 
 # ✅Todo List
 
-- [ ] 亮暗模式切换
-- [ ] 每日一言API的接入（或者其他倒计时类的API接入）
+- [x] 亮暗模式切换
+- [ ] 接入GPT实现歌曲推荐
 - [ ] 歌单个性化推荐
 
---
+++ main.py
@@ -0,0 +1,556 @@
+from PyQt5 import QtWidgets,QtGui,QtCore
+from PyQt5.QtMultimedia import QMediaContent,QMediaPlayer
+import qtawesome as qta
+import requests
+
+
+font=QtGui.QFont()
+song_font = QtGui.QFont()
+font.setFamily("STXihei")
+font.setPointSize(20)
+song_font.setFamily("STXihei")
+song_font.setPointSize(14)
+
+
+class ToggleButton(QtWidgets.QWidget):
+    checkedChanged = QtCore.pyqtSignal(bool)
+    def __init__(self,parent=None):
+        super(QtWidgets.QWidget, self).__init__(parent)
+
+        self.checked = False
+        self.bgColorOff = QtGui.QColor(230, 230, 230)
+        self.bgColorOn = QtGui.QColor(60, 60, 60)
+
+        self.sliderColorOff = QtGui.QColor(100, 100, 100)
+        self.sliderColorOn = QtGui.QColor(100, 184, 255)
+
+        self.textColorOff = QtGui.QColor(143, 143, 143)
+        self.textColorOn = QtGui.QColor(255, 255, 255)
+
+        self.textOff = "OFF"
+        self.textOn = "ON"
+
+        self.space = 2
+        self.rectRadius = 5
+
+        self.step = self.width() / 50
+        self.startX = 0
+        self.endX = 0
+
+        self.timer = QtCore.QTimer(self)  # 初始化一个定时器
+        self.timer.timeout.connect(self.updateValue)  # 计时结束调用operate()方法
+
+        #self.timer.start(5)  # 设置计时间隔并启动
+
+        self.setFont(QtGui.QFont("Microsoft Yahei", 10))
+
+        #self.resize(55,22)
+
+    def updateValue(self):
+        if self.checked:
+            if self.startX < self.endX:
+                self.startX = self.startX + self.step
+            else:
+                self.startX = self.endX
+                self.timer.stop()
+        else:
+            if self.startX  > self.endX:
+                self.startX = self.startX - self.step
+            else:
+                self.startX = self.endX
+                self.timer.stop()
+
+        self.update()
+
+
+    def mousePressEvent(self,event):
+        self.checked = not self.checked
+        #发射信号
+        self.checkedChanged.emit(self.checked)
+
+        # 每次移动的步长为宽度的50分之一
+        self.step = self.width() / 50
+        #状态切换改变后自动计算终点坐标
+        if self.checked:
+            self.endX = self.width() - self.height()
+        else:
+            self.endX = 0
+        self.timer.start(5)
+
+    def paintEvent(self, evt):
+        #绘制准备工作, 启用反锯齿
+            painter = QtGui.QPainter()
+
+
+
+            painter.begin(self)
+
+            painter.setRenderHint(QtGui.QPainter.Antialiasing)
+
+
+            #绘制背景
+            self.drawBg(evt, painter)
+            #绘制滑块
+            self.drawSlider(evt, painter)
+            #绘制文字
+            self.drawText(evt, painter)
+
+            painter.end()
+
+
+    def drawText(self, event, painter):
+        painter.save()
+
+        if self.checked:
+            painter.setPen(self.textColorOn)
+            painter.drawText(0, 0, int(self.width() / 2 + self.space * 2), int(self.height()), QtCore.Qt.AlignCenter, self.textOn)
+        else:
+            painter.setPen(self.textColorOff)
+            painter.drawText(int(self.width() / 2), 0,int(self.width() / 2 - self.space), int(self.height()), QtCore.Qt.AlignCenter, self.textOff)
+
+        painter.restore()
+
+
+    def drawBg(self, event, painter):
+        painter.save()
+        painter.setPen(QtCore.Qt.NoPen)
+
+        if self.checked:
+            painter.setBrush(self.bgColorOn)
+        else:
+            painter.setBrush(self.bgColorOff)
+
+        rect = QtCore.QRect(0, 0, int(self.width()), int(self.height()))
+        #半径为高度的一半
+        radius = rect.height() / 2
+        #圆的宽度为高度
+        circleWidth = rect.height()
+
+        path = QtGui.QPainterPath()
+        path.moveTo(radius, rect.left())
+        path.arcTo(QtCore.QRectF(rect.left(), rect.top(), circleWidth, circleWidth), 90, 180)
+        path.lineTo(rect.width() - radius, rect.height())
+        path.arcTo(QtCore.QRectF(rect.width() - rect.height(), rect.top(), circleWidth, circleWidth), 270, 180)
+        path.lineTo(radius, rect.top())
+
+        painter.drawPath(path)
+        painter.restore()
+
+    def drawSlider(self, event, painter):
+        painter.save()
+
+        if self.checked:
+            painter.setBrush(self.sliderColorOn)
+        else:
+            painter.setBrush(self.sliderColorOff)
+
+        rect = QtCore.QRect(0, 0, self.width(), self.height())
+        sliderWidth = rect.height() - self.space * 2
+        sliderRect = QtCore.QRect(int(self.startX + self.space), int(self.space), sliderWidth, sliderWidth)
+        painter.drawEllipse(sliderRect)
+
+        painter.restore()
+
+class Music(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.var_song_name = ''
+        self.setFixedSize(500,250)
+        self.setWindowTitle("🎧+📚=😊")
+        self.setWindowOpacity(0.75)
+
+        self.init_ui()
+        self.custom_style()
+        self.mode = 'False'         # 初始UI设置为光亮模式
+        self.play_status = False    # 播放状态初始化为否
+        self.player = QMediaPlayer(self)
+
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(1000)
+        self.timer.start()
+        self.timer.timeout.connect(self.check_music_status)
+
+    # 设置样式
+    def custom_style(self):
+        self.setStyleSheet('''
+            #main_widget{
+                
+                background:white;
+            }
+            #play_btn,#next_btn{
+                border:none;
+            }
+            #play_btn:hover,#next_btn:hover{
+                background:	#696969;
+                border-radius:5px;
+                
+            }
+        ''')
+        self.close_btn.setStyleSheet('''
+            QPushButton{
+                background:#F76677;
+                border-radius:5px;
+                }
+            QPushButton:hover{
+                background:red;
+                }''')
+        self.status_label.setStyleSheet('''
+            QLabel{
+                background:#FFD85A;
+                border-radius:5px;
+                }
+        ''')
+
+    def init_ui(self):
+        
+        # 窗口布局
+        self.main_widget = QtWidgets.QWidget()
+        self.main_widget.setObjectName("main_widget")
+        self.main_layout = QtWidgets.QGridLayout()
+        self.main_widget.setLayout(self.main_layout)
+
+        # 标题
+        self.title_lable = QtWidgets.QLabel('🌥WindPlayer')
+        self.title_lable.setFont(font)
+        self.title_lable.setStyleSheet('''
+        QLabel{
+                color:"black";
+                }
+        ''')
+        self.title_lable.setFont(font)
+        self.title_lable.setAlignment(QtCore.Qt.AlignCenter)
+
+        # About 
+        about_icon = qta.icon("fa.info-circle")
+        self.about_btn = QtWidgets.QPushButton(about_icon,"")
+        self.about_btn.setIconSize(QtCore.QSize(40,40))
+        self.about_btn.setFixedSize(40,40)
+        self.about_btn.setObjectName("about_btn")
+        self.about_btn.clicked.connect(self.show_info)
+        self.about_btn.setStyleSheet('''
+            #about_btn{
+                background:white;
+                border-radius:5px;
+            }
+            #about_btn:hover{
+                background:	#e9e9e9;
+            }
+        ''')
+        # Toggle Button
+        self.switchBtn = ToggleButton(self)
+        self.switchBtn.checkedChanged.connect(self.switch_mode)
+
+        # 关闭按钮
+        self.close_btn = QtWidgets.QPushButton("")  # 关闭按钮
+        self.close_btn.clicked.connect(self.close_btn_event)
+        self.close_btn.setFixedSize(15,15)
+
+        # 音乐状态按钮
+        self.status_label = QtWidgets.QLabel("")
+        self.status_label.setFixedSize(15,15)
+
+        # 播放按钮
+        play_icon = qta.icon("fa.play-circle-o",)
+        self.play_btn = QtWidgets.QPushButton(play_icon,"")
+        self.play_btn.setIconSize(QtCore.QSize(50, 50))
+        self.play_btn.setFixedSize(82,82)
+        self.play_btn.setObjectName("play_btn")
+        self.play_btn.clicked.connect(self.play_music)
+        self.play_btn.setStyleSheet('''
+                    #play_btn{
+                        background:white;
+                        border-radius:5px;
+                        }
+                    #play_btn:hover{
+                        background:	#e9e9e9;
+                        }''')
+
+
+        # 下一首按钮
+        next_icon = qta.icon("fa.chevron-right")
+        self.next_btn = QtWidgets.QPushButton(next_icon,"")
+        self.next_btn.setIconSize(QtCore.QSize(50,50))
+        self.next_btn.setFixedSize(82,82)
+        self.next_btn.setObjectName("next_btn")
+        self.next_btn.clicked.connect(self.next_music)
+        self.next_btn.setStyleSheet('''
+                    #next_btn{
+                        background:white;
+                        border-radius:5px;
+                        }
+                    #next_btn:hover{
+                        background:	#e9e9e9;
+                        }''')
+
+        # 进度条
+        self.process_bar = QtWidgets.QProgressBar()
+        self.process_value = 0
+        self.process_bar.setValue(self.process_value)
+        self.process_bar.setFixedHeight(5)
+        self.process_bar.setTextVisible(False)
+        self.process_bar.setStyleSheet('''QProgressBar {   border: 2px solid grey;   border-radius: 5px;   background-color: #FFFFFF;}QProgressBar::chunk {   background-color: #05B8CC;   width: 28px;}QProgressBar {   border: 2px solid grey;   border-radius: 5px;   text-align: center;}
+        ''')
+
+        # 播放信息
+        self.song_name = QtWidgets.QLabel('Nothing Playing')
+        self.song_name.setFont(song_font)
+
+
+        self.main_layout.addWidget(self.close_btn,0,0,1,1)
+        self.main_layout.addWidget(self.title_lable,0,1,1,1)
+        self.main_layout.addWidget(self.about_btn, 0, 2, 1, 1)
+        self.main_layout.addWidget(self.switchBtn, 0,3,1,1)
+        self.main_layout.addWidget(self.status_label,1,0,1,1)
+        self.main_layout.addWidget(self.play_btn, 1, 1, 1, 1)
+        self.main_layout.addWidget(self.next_btn, 1, 3, 1, 1)
+        self.main_layout.addWidget(self.process_bar,2,0,1,4)
+        self.main_layout.addWidget(self.song_name,3,0,1,4)
+        
+        self.setCentralWidget(self.main_widget)
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)  # 隐藏边框
+
+    # 关闭程序
+    def close_btn_event(self):
+        self.close()
+
+    # About 弹窗
+    def show_info(self):
+        reply =  QtWidgets.QMessageBox.information(self, "💡About", r'Made by: <a href = "https://github.com/Dafeigy/">Cybersh1t</a><br>Issue reporting: <a href = "https://github.com/Dafeigy/WindMusic/issues/">Github</a><br>Email: <a href = "mailto:cybercolyce@gmail.com">GMail</a><br>')
+    # 夜间模式切换
+    def switch_mode(self, checked):
+        if checked:
+            self.main_widget.setStyleSheet('''
+            #main_widget{
+                
+                background:#202020;
+            }
+            ''')
+            self.play_btn.setStyleSheet(
+                '''
+            #play_btn{
+                border:none;
+                background: #202020;
+            }
+            #play_btn:hover{
+                background:	#323232;
+                border-radius:5px;
+            }
+                '''
+            )
+            self.next_btn.setStyleSheet('''
+                    #next_btn{
+                        background:#202020;
+                        border-radius:5px;
+                        }
+                    #next_btn:hover{
+                        background:	#323232;
+                        }''')
+            self.title_lable.setStyleSheet(
+                '''
+            QLabel{
+            color:white;
+                }
+                '''
+            )
+            self.song_name.setStyleSheet(
+                '''
+            QLabel{
+            color:white;
+                }
+                ''')
+            self.about_btn.setStyleSheet('''
+                #about_btn{
+                    border:none;
+                    background: #202020;
+            }
+                #about_btn:hover{
+                    background:	#323232;
+                    border-radius:5px;
+            }'''
+            )
+            
+        else:
+            self.main_widget.setStyleSheet('''
+            #main_widget{
+                background:white;
+            }
+            ''')
+            self.play_btn.setStyleSheet(
+                '''
+            #play_btn{
+                border:none;
+                background: white;
+            }
+            #play_btn:hover{
+                background:	#e9e9e9;
+                border-radius:5px;
+            }
+                '''
+            )
+            self.next_btn.setStyleSheet('''
+                    #next_btn{
+                        background:white;
+                        border-radius:5px;
+                        }
+                    #next_btn:hover{
+                        background:	#e9e9e9;
+                        }''')
+            self.title_lable.setStyleSheet(
+                '''
+            QLabel{
+            color:black;
+                }
+                ''')
+            self.song_name.setStyleSheet(
+                '''
+            QLabel{
+            color:black;
+                }
+                ''')
+            self.about_btn.setStyleSheet(
+                '''
+                #about_btn{
+                    border:none;
+                    background: white;
+            }
+                #about_btn:hover{
+                    background:	#e9e9e9;
+            }'''
+            )
+    # 鼠标长按事件
+    def mousePressEvent(self, event):
+        if event.button() == QtCore.Qt.LeftButton:
+            self.m_drag = True
+            self.m_DragPosition = event.globalPos() - self.pos()
+            event.accept()
+            self.setCursor(QtGui.QCursor(QtCore.Qt.OpenHandCursor))
+
+    # 鼠标移动事件
+    def mouseMoveEvent(self, QMouseEvent):
+        if QtCore.Qt.LeftButton and self.m_drag:
+            self.move(QMouseEvent.globalPos() - self.m_DragPosition)
+            QMouseEvent.accept()
+
+    # 鼠标释放事件
+    def mouseReleaseEvent(self, QMouseEvent):
+        self.m_drag = False
+        self.setCursor(QtGui.QCursor(QtCore.Qt.ArrowCursor))
+
+    # 播放音乐
+    def play_music(self):
+        try:
+            # 播放音乐
+            if self.play_status is False:
+                self.play_status = True # 设置播放状态为是
+                self.play_btn.setIcon(qta.icon("fa.pause-circle-o")) # 设置播放图标
+                player_status = self.player.mediaStatus() # 获取播放器状态
+                # print("当前播放状态：",player_status)
+                if player_status == 6:
+                    # 设置状态标签为绿色
+                    self.status_label.setStyleSheet('''QLabel{background:#72EA72;border-radius:5px;}''')
+                    self.player.play()
+                    self.song_name.setText(f'{self.var_song_name} - Playing')
+                else:
+                    self.next_music()
+                    self.song_name.setText(f'{self.var_song_name} - Playing')
+            # 暂停音乐
+            else:
+                # 设置状态为蓝色
+                self.status_label.setStyleSheet('''QLabel{background:#0099CC;border-radius:5px;}''')
+                self.play_status = False
+                self.play_btn.setIcon(qta.icon("fa.play-circle-o"))
+                self.player.pause()
+                self.song_name.setText(f'{self.var_song_name} - Paused')
+        except Exception as e:
+            pass
+
+    # 下一首音乐
+    def next_music(self):
+        try:
+            # 设置状态为黄色
+            self.status_label.setStyleSheet('''
+                QLabel{
+                    background:#FFBF00;
+                    border-radius:5px;
+                    }
+            ''')
+            self.play_status = True # 设置播放状态为是
+            self.play_btn.setIcon(qta.icon("fa.pause-circle-o")) # 修改播放图标
+            self.process_value = 0 # 重置进度值
+
+            # 获取网络歌曲
+            self.get_music_thread = GetMusicThread()
+            self.get_music_thread.finished_signal.connect(self.init_player)
+            self.get_music_thread.start()
+        except Exception as e:
+            pass
+
+    # 设置播放器
+    def init_player(self,url, song_name):
+        
+        self.var_song_name = song_name
+        self.song_name.setText(f'{self.var_song_name} - Playing')
+        content = QMediaContent(QtCore.QUrl(url))
+        self.player.setMedia(content)
+        self.player.setVolume(50)
+
+        self.player.play()
+        self.duration = self.player.duration()  # 音乐的时长
+        # 设置状态为绿色
+        self.status_label.setStyleSheet('''
+            QLabel{
+                background:#72EA72;
+                border-radius:5px;
+                }
+        ''')
+
+        # 进度条计时器
+        self.process_timer = QtCore.QTimer()
+        self.process_timer.setInterval(1000)
+        self.process_timer.start()
+        self.process_timer.timeout.connect(self.process_timer_status)
+
+
+    # 定时器
+    def check_music_status(self):
+        player_status = self.player.mediaStatus()
+        player_duration = self.player.duration()
+        # print("音乐时间：",player_duration)
+        # print("当前播放器状态",player_status)
+        if player_status == 7:
+            self.next_music()
+
+        if player_duration > 0:
+            self.duration = player_duration
+
+    # 进度条定时器
+    def process_timer_status(self):
+        try:
+            if self.play_status is True:
+                self.process_value +=float(100 / (self.duration/1000))
+                self.process_bar.setValue(int(self.process_value))
+        except Exception as e:
+            pass
+
+
+# 异步子线程获取音乐链接
+class GetMusicThread(QtCore.QThread):
+    finished_signal = QtCore.pyqtSignal(str,str)
+    def __init__(self,parent=None):
+        super().__init__(parent)
+    def run(self):
+        reps = requests.post("https://api.uomg.com/api/rand.music?sort=抖音榜&format=json")
+        file_url = reps.json()['data']['url']
+        song_name = reps.json()['data']['name']
+        self.finished_signal.emit(file_url,song_name)
+
+
+def main():
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    gui = Music()
+    gui.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
